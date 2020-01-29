840c00f8e2fe564c9452f7592b49fefbecede064
Erelys <59955803+Erelys@users.noreply.github.com>
2020-01-29 01:10:39 +0100

    Mise en forme du code pour 3 persos et 3 monstres
    J'ai commencé à mettre en place chaque switch avec des printf pour voir si ça fonctionne.
    
    Co-Authored-By: Aiden P-L. <aidenosaur@users.noreply.github.com>

--- Combat.c
+++ Combat.c
@@ -6,125 +6,185 @@ struct Characteristic{
 	int damage;
 	int mana;
 	int range;
+	int state;
 };
 
 typedef struct Characteristic characteristic ;
 
 int main(){
 
-    int varDamage = 25;
-    int varMonsterDamage = 30;
-    int actionPlayer;
+		int actionShaman;
+		int actionArcher;
+		int actionWarrior;
 
-    int maxHealth = 200;
-    int curMonsterLp = 200;
-    int curHealth = maxHealth;
-
-    int manaMonster = 50;
-    int manaPlayer = 50;
+		int actionMonster1;
+		int actionMonster2;
+		int actionMonster3;
 
     int useMana = 10;
     int poison = 5;
     int antidote;
 
-    int monsterState;
-    int playerState;
+
+
 	//Structure des personnages
 
   characteristic shaman;
 		shaman.lp=150;
 		shaman.mana=100;
 		shaman.range=5;
+		shaman.state;
 
 
 	characteristic warrior;
 		warrior.lp=200;
 		warrior.damage=35;
 		warrior.range=5;
+		warrior.state;
 
 	characteristic archer;
 		archer.lp=150;
 		archer.damage=20;
 		archer.mana=50;
 		archer.range=10;
+		archer.state;
 
-	printf("---SHAMAN---\n Life Point : %d\n Mana : %d\n Range : %d\n\n The Chaman have the capacity to heal himslef or his friend by using an antidote.\n He can also poisoned a monster by using a poisoned\n\n", chaman);
+	characteristic team;
+		team.lp = shaman.lp + warrior.lp + archer.lp;
+		team.mana = shaman.mana + archer.mana;
+		team.damage = warrior.damage + archer.damage;
+
+	printf("---SHAMAN---\n Life Point : %d\n Mana : %d\n Range : %d\n\n The Chaman have the capacity to heal himslef or his friend by using an antidote.\n He can also poisoned a monster by using a poisoned\n\n", shaman);
 
 	printf("---WARRIOR---\n Life Point : %d\n Attack : %d\n Range : %d\n\n The warrior have the capacity to attack only one monster per turn but his strenght is stronger than the other\n He can also defend himself or his friend with his shield\n\n", warrior);
 
 	printf("---ARCHER---\n Life Point : %d\n Attack : %d\n Mana : %d\n Range : %d\n\n The archer have the capacity to use his bow to attack either a single monster or to attack several monster", archer);
 
+	characteristic monsters;
+			monsters.lp=200;
+			monsters.damage=30;
+			monsters.mana=50;
+			monsters.range=5;
+			monsters.state;
+
+	characteristic monster1;
+			monster1.lp=200;
+			monster1.damage=30;
+			monster1.mana=50;
+			monster1.range=5;
+
+	characteristic monster2;
+			monster2.lp=200;
+			monster2.damage=30;
+			monster2.mana=50;
+			monster2.range=5;
+
+	characteristic monster3;
+			monster3.lp=200;
+			monster3.damage=30;
+			monster3.mana=50;
+			monster3.range=5;
 
 
 
 
-    printf ("\n""You start the game with 200 life points""\n\n""Here is a monster with 200 life point, Defeat him !""\n\n""Enter 1 to attack the monster""\n\n""2 to hide yourself behind a bloc of rock""\n\n""3 to use a special poison attack\n\n""or 4 to heal yourself \n\n" );
+	    printf ("THREE monsters appears !! Defeat them !""\n\n""---MONSTER---\n Life Point : %d\n Attack : %d\n Mana : %d\n Range : %d\n\n", monsters);
 
-
-
-
-     while (curMonsterLp > 0 && curHealth > 0){
+     while (monsters.lp > 0 && team.lp > 0){
 
           //On essaie de tirer les actions du monstre au hasard en faisant 1 ou 2
-                int monsterBehaviour = rand()%3 + 1;
-                if (manaMonster < 10 && monsterBehaviour == 3){
-                monsterBehaviour = rand()%2 + 1;
+                int actionMonster1 = rand()%3 + 1;
+                if (monster1.mana < 10 && actionMonster1 == 3){
+                actionMonster1 = rand()%2 + 1;
+                }
+
+								int actionMonster2 = rand()%3 + 1;
+                if (monster2.mana < 10 && actionMonster2 == 3){
+                actionMonster2 = rand()%2 + 1;
+								}
+
+								int actionMonster3 = rand()%3 + 1;
+                if (monster3.mana < 10 && actionMonster3 == 3){
+                actionMonster3 = rand()%2 + 1;
                 }
          //Une fois par tour le monstre perdra 5pv que si le joueur l'a empoisonné une fois
 
 
-                printf("ManaPlayer = %d \n\n",manaPlayer);
-                printf("ManaMonster = %d \n\n",manaMonster);
+                printf("Mana Shaman = %d Mana Archer %d \n\n", shaman.mana, archer.mana);
 
-                if (monsterState == 40 && manaPlayer >= 10){
+
+              /*  if (monsterState == 40 && manaPlayer >= 10){
                     manaPlayer = manaPlayer - useMana;
                     curMonsterLp = curMonsterLp - poison;
                     printf("The monster is poisoned, he lost 5Lp \n, he is now %d\n",curMonsterLp);
-                }
+
 
                 if (playerState == 50 && manaMonster >= 10){
                     manaMonster = manaMonster - useMana;
                     curHealth = curHealth - poison;
                     printf("You are poisoned, you lost 5Lp \n, you're now %d",curHealth);
-                }
+                } */
 
                 printf("__________________________________""\n");
 
 
-                 scanf ("%d", &actionPlayer);
+                 scanf ("%d" "%d""%d", &actionArcher, &actionShaman, &actionWarrior);
                  printf ("\n");
 
-                switch (actionPlayer){
+                switch (actionWarrior){
 
                 default:
                 printf("You didn't tape a correct number, nothing happened");
                 break;
 
                 case 1:
-                curMonsterLp = curMonsterLp - varDamage;
-                printf("You inflict 25 damage to the monster, keep going!""\n""There's %d left""\n""\n", curMonsterLp);
+                printf("You inflict 25 damage to the monster, keep going!""\n""There's %d left""\n""\n");
                 break;
 
                 case 2:
-                printf("You are behind a wall\n",curHealth);
+                printf("You are behind a wall\n");
                 break;
 
-                case 3:
+                }
+
+								switch (actionArcher){
+
+                default:
+                printf("You didn't tape a correct number, nothing happened");
+                break;
+
+                case 1:
+                printf("You inflict 25 damage to the monster, keep going!""\n""There's %d left""\n""\n");
+                break;
+
+                case 2:
+                printf("You are behind a wall\n");
+                break;
+							}
+
+								switch (actionShaman){
+
+                default:
+                printf("You didn't tape a correct number, nothing happened");
+                break;
+
+                case 1:
                 printf("You poisoned the monster he will loose 5lp per turn \n");
-                monsterState = 40;
+
                 break;
 
-                case 4:
-                if (playerState == 50 && manaPlayer >= 10){
-                    playerState = 0;
+                case 2:
+
+                if (team.state == 50 && shaman.mana >= 10){
+                    team.state = 0;
                     printf("You're healed !\n\n");
                 }
                 break;
 
-                }
+              }
 
 
-                switch (monsterBehaviour){
+              /*  switch (actionMonster1){
                 case 1:
                 if(actionPlayer == 5){
                 curHealth = curHealth - 7,5;
@@ -156,14 +216,14 @@ int main(){
 
 
 
-                    }
+							} */
 
-         manaPlayer = manaPlayer + 1;
+         team.mana = team.mana + 1;
 
              }
 
 
-         if (curMonsterLp <= 0){
+         /*if (curMonsterLp <= 0){
 
            printf ("The monster is now dead, congratulation !");
         }
@@ -173,5 +233,5 @@ int main(){
 
         return 0;
 
-        }
+			}*/
 }
